932fe9be139f35133cbc465668ecb89923b88d0f
User <pamudiramkumar@gmail.com>
2020-03-27 22:40:27 +0530

    Initial commit

---
+++ app/src/main/java/com/example/labsheet4/Main2Activity.java
@@ -0,0 +1,33 @@
+package com.example.labsheet4;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Main2Activity extends AppCompatActivity {
+    private static final String CHANNEL_ID = "100CH";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        Button cancelButton = (Button) findViewById(R.id.button2);
+
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent cancelIntent = new Intent(Main2Activity.this, MainActivity.class);
+                startActivity(cancelIntent);
+            }
+        });
+    }
+}
